9c5f8653fd4dd596e7e884693e8e0416a09075e7
Benoît S <bserie@evolix.fr>
2018-05-28 11:49:57 +0200

    IS_EVOACME_LIVELINKS: Only executed if evoacme installed

--- evocheck.sh
+++ evocheck.sh
@@ -769,18 +769,20 @@ if [ -e /etc/debian_version ]; then
     fi
 
     if [ "$IS_EVOACME_LIVELINKS" = 1 ]; then
-        for live in /etc/letsencrypt/*/live; do
-            actualLink=$(ls -lhad $live | tr -s ' ' | cut -d' ' -f 11)
-            actualCertDate=$(cut -d'/' -f5 <<< $actualLink)
-            liveDir=$(ls -lhad $live | tr -s ' ' | cut -d' ' -f 9)
-            certDir=${liveDir%%/live}
-            lastCertDir=$(stat -c %n ${certDir}/[0-9]* | tail -1)
-            lastCertDate=$(cut -d'/' -f5 <<< $lastCertDir)
-            if [[ "$actualCertDate" != "$lastCertDate" ]]; then
-                echo 'IS_EVOACME_LIVELINKS FAILED!'
-                break
-            fi
-        done
+        if [ -x "$(which evoacme)" ]; then
+            for live in /etc/letsencrypt/*/live; do
+                actualLink=$(ls -lhad $live | tr -s ' ' | cut -d' ' -f 11)
+                actualCertDate=$(cut -d'/' -f5 <<< $actualLink)
+                liveDir=$(ls -lhad $live | tr -s ' ' | cut -d' ' -f 9)
+                certDir=${liveDir%%/live}
+                lastCertDir=$(stat -c %n ${certDir}/[0-9]* | tail -1)
+                lastCertDate=$(cut -d'/' -f5 <<< $lastCertDir)
+                if [[ "$actualCertDate" != "$lastCertDate" ]]; then
+                    echo 'IS_EVOACME_LIVELINKS FAILED!'
+                    break
+                fi
+            done
+        fi
     fi
 fi
 

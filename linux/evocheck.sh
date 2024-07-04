Evolix/evocheck

commit c4c8dd2112e36596ad179d47f0c2b3ce265f199e

IS_SYSLOGCONF: better detection

linux/evocheck.sh
@@ -193,7 +193,7 @@ check_logrotateconf() {
     test -e /etc/logrotate.d/zsyslog || failed "IS_LOGROTATECONF" "missing zsyslog in logrotate.d"
 }
 check_syslogconf() {
-    grep -q "^# Syslog for Pack Evolix serveur" /etc/*syslog.conf \
+    grep --quiet --ignore-case "^Syslog for Pack Evolix" /etc/*syslog*/*.conf \
         || failed "IS_SYSLOGCONF" "syslog evolix config file missing"
 }
 check_debiansecurity() {

commit 4c0d1a6933a3b893a7742574dedd94e19fc58820

IS_SYSLOGCONF: better better detection
Fix regression introduced in c4c8dd2112

linux/evocheck.sh
@@ -198,7 +198,7 @@ check_syslogconf() {
     # Test for modern servers
     if [ ! -f /etc/rsyslog.d/10-evolinux-default.conf ]; then
         # Fallback test for legacy servers
-        if ! grep --quiet --ignore-case "Syslog for Pack Evolix" /etc/*syslog*/*.conf; then
+        if ! grep --quiet --ignore-case "Syslog for Pack Evolix" /etc/*syslog*/*.conf /etc/*syslog.conf; then
             failed "IS_SYSLOGCONF" "Evolix syslog config is missing"
         fi
     fi
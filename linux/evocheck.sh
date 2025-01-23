9bce380a3362d3985469ed21c58ce03ec4bb98dd
David Prevot <dprevot+git@evolix.fr>
2025-01-23 11:51:06 +0100

    Revert "Add IS_NRPEPRESSURE check"
    This reverts commit 040fe95d71d90ac4b34a0aa33fae490dd107253d.

--- linux/evocheck.sh
+++ linux/evocheck.sh
@@ -1601,17 +1601,6 @@ check_versions() {
         fi
     done
 }
-check_nrpepressure() {
-    # Taken from detect_os function
-    DEBIAN_MAIN_VERSION=$(cut -d "." -f 1 < /etc/debian_version)
-    if [ "${DEBIAN_MAIN_VERSION}" -ge 12 ]; then
-        monitoringctl status pressure_cpu > /dev/null 2>&1
-        rc="$?"
-        if [ "${rc}" -ne 0 ]; then
-            failed "IS_NRPEPRESSURE" "pressure_cpu check not defined or monitoringctl not correctly installed"
-        fi
-    fi
-}
 
 main() {
     # Default return code : 0 = no error
@@ -1747,7 +1736,6 @@ main() {
     test "${IS_LXC_PHP_BAD_DEBIAN_VERSION:=1}" = 1 && check_lxc_php_bad_debian_version
     test "${IS_LXC_OPENSSH:=1}" = 1 && check_lxc_openssh
     test "${IS_CHECK_VERSIONS:=1}" = 1 && check_versions
-    test "${IS_NRPEPRESSURE:=1}" = 1 && check_nrpepressure
 
     if [ -f "${main_output_file}" ]; then
         lines_found=$(wc -l < "${main_output_file}")

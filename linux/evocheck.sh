611dd87833a708a88969c08761d8ba1e41d7533c
David Prevot <dprevot+git@evolix.fr>
2025-04-03 07:07:20 +0200

    Add IS_NO_SIGNED_BY check

--- linux/evocheck.sh
+++ linux/evocheck.sh
@@ -287,6 +287,18 @@ check_not_deb822() {
 	done
     fi
 }
+check_no_signed_by() {
+    if { ! is_debian_buster && ! is_debian_bullseye ; }; then
+        for source in /etc/apt/sources.list.d/*.sources; do
+            if [ -f "${source}" ]; then
+		( grep --quiet '^Signed-by' "${source}" && \
+                    failed "IS_NO_SIGNED_BY" "${source} contains a Source-by entry that should be capitalized as Signed-By" ) || \
+		( grep --quiet '^Signed-By' "${source}" || \
+                    failed "IS_NO_SIGNED_BY" "${source} has no Signed-By entry" )
+	    fi
+	done
+    fi
+}
 check_aptitude() {
     test -e /usr/bin/aptitude && failed "IS_APTITUDE" "aptitude may not be installed on Debian >=8"
 }
@@ -1723,6 +1735,7 @@ main() {
     test "${IS_SURY:=1}" = 1 && check_sury
     test "${IS_SURY_LXC:=1}" = 1 && check_sury_lxc
     test "${IS_NOT_DEB822:=1}" = 1 && check_not_deb822
+    test "${IS_NO_SIGNED_BY:=1}" = 1 && check_no_signed_by
     test "${IS_APTITUDE:=1}" = 1 && check_aptitude
     test "${IS_APTGETBAK:=1}" = 1 && check_aptgetbak
     test "${IS_USRRO:=1}" = 1 && check_usrro

e521d412217db85e5dc4fc0e7cc785763ce16eb5
David Prevot <dprevot+git@evolix.fr>
2025-12-04 10:37:24 +0100

    get_version: avoid stderr if minifirewall version can’t be guessed on wheezy

--- linux/evocheck.wheezy.sh
+++ linux/evocheck.wheezy.sh
@@ -1014,7 +1014,9 @@ get_version() {
             grep '^VERSION=' "${command}" | head -1 | cut -d '=' -f 2
             ;;
         minifirewall)
-            ${command} version | head -1 | cut -d ' ' -f 3
+            if $(${command} version); then
+                ${command} version | head -1 | cut -d ' ' -f 3
+            fi
             ;;
         ## Let's try the --version flag before falling back to grep for the constant
         kvmstats)
